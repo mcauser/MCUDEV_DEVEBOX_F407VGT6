aa9a110c9bd557da41607541a1d39b1fffeb034e
Mike Causer <mcauser@gmail.com>
2019-10-09 11:01:48 +1100

    stm32: In link script, define start of stack separately from heap end.
    As per https://github.com/micropython/micropython/commit/14cf91f70467aa928f3e17223f108ace0864b4fe#diff-9e25c626665fd91147d4a94a0913151f

--- mpconfigboard.mk
+++ mpconfigboard.mk
@@ -1,6 +1,6 @@
 MCU_SERIES = f4
 CMSIS_MCU = STM32F407xx
 AF_FILE = boards/stm32f405_af.csv
-LD_FILES = boards/MCUDEV_DEVEBOX_F407VGT6/stm32f407vg.ld boards/common_ifs.ld
+LD_FILES = boards/stm32f405.ld boards/common_ifs.ld
 TEXT0_ADDR = 0x08000000
 TEXT1_ADDR = 0x08020000
